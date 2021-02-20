552e049444413af01707bf37f2ca697c58e473f5
micheledemarco93 <micheledemarco93@gmail.com>
2021-02-20 16:22:08 +0100

    new home - review struct

---
+++ lib/screens/camping/homeCamping.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeCamping extends StatefulWidget {
+  @override
+  _HomeCamping createState() => _HomeCamping();
+}
+
+class _HomeCamping extends State<HomeCamping>
+    with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: null,
+    );
+  }
+}
